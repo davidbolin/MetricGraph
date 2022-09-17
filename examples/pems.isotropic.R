69fa6f572cf5db9cd733c6956a691d6871b036c0
davidbolin <davidbolin@gmail.com>
2022-09-17 16:37:53 +0300

    update cross validation

--- examples/pems.isotropic.R
+++ examples/pems.isotropic.R
@@ -28,45 +28,44 @@ graph$observation_to_vertex()
 
 # Fit isotropic model
 graph$compute_resdist()
+
 like.exp <- function(theta,graph){
-  Sigma <- theta[1] ^2*exp(-theta[2]*graph$res.dist[graph$PtV,graph$PtV]) +
-    theta[3]^2*diag(rep(1,length(graph$y)))
+  Sigma <- theta[3]^2*exp(-theta[2]*graph$res.dist[graph$PtV,graph$PtV]) +
+    theta[1]^2*diag(rep(1,length(graph$y)))
   R <- chol(Sigma)
   return(-sum(log(diag(R))) - 0.5*t(graph$y)%*%solve(Sigma,graph$y))
 }
 theta <- c(1,1,1)
 res <- optim(log(theta), function(x) -like.exp(exp(x),graph))
-theta <- exp(res$par)
-Sigma <- theta[1]^2*exp(-theta[2]*graph$res.dist)
-Sigma.i <- theta[1]^2*exp(-theta[2]*graph$res.dist[graph$PtV,graph$PtV])
-Sigma.o <- theta[1]^2*exp(-theta[2]*graph$res.dist[graph$PtV,graph$PtV]) + theta[3]^2*diag(rep(1,length(graph$y)))
-
-mu.p <- Sigma[,graph$PtV]%*%solve(Sigma.o,graph$y)
-y_f <- mu.p[graph$PtV]
-y_L_iso <- rep(0, length(graph$y))
-for(i in 1:length(graph$PtV)){
-  mu.p_i <- Sigma[,graph$PtV[-i]]%*%solve(Sigma.o[-i,-i],graph$y[-i])
-  y_L_iso[i] <- mu.p_i[graph$PtV[i]]
-}
-cat('var res= ',var(graph$y-y_L_iso),'\n')
-fig <- plot_obs(graph,y_L_iso, y_loc = Y_loc) + scale_colour_gradientn(colours = heat.colors(10))
-print(fig)
+theta.res <- exp(res$par)
 
 # Fit alpha=1 model
 theta.exp <- c(9.4726902736, 0.0001559032, 0.3745814561)
 res <- optim(log(theta.exp), function(x) -likelihood.exp.graph.v2(exp(x),graph) )
 theta.exp <- exp(res$par)
-Y_L_exp <- posterior.leave.v2(theta.exp,graph)
-cat('var exp= ',var(Y_L_exp-graph$y),'\n')
-fig <- plot_obs(graph,Y_L_exp, y_loc = Y_loc) + scale_colour_gradientn(colours = heat.colors(10))
-print(fig)
 
 # Fit graph Laplace model
 graph$compute_laplacian()
 theta.graph <- c(9.478923, 7.154005, 1.160011e-05)
 res <- optim(log(theta.graph), function(x) -likelihood.graph_laplacian(exp(x),graph) )
 theta.graph <- exp(res$par)
-Y_L_graph <- posterior.leave.graph_laplacian(theta.graph,graph)
-cat('var graph= ',var(Y_L_graph-graph$y),'\n')
+
+#cross validation
+K <- 5 #number of groups in cross validation
+n.y <- length(graph$y)
+n.g <- floor(n.y/K)
+ind <- rep(1:K,n.g+1)[1:n.y]
+ind <- ind[sample(1:n.y,n.y)]
+
+y_L_iso <- posterior.crossvalidation(theta.res, graph, model = "isoExp",ind = ind)
+Y_L_exp <- posterior.crossvalidation(theta.exp,graph, model = "alpha1",ind = ind)
+Y_L_graph <- posterior.crossvalidation(theta.graph,graph, model ="GL",ind = ind)
+result <- data.frame(MSE = c(var(graph$y-y_L_iso),
+                            var(Y_L_exp-graph$y),
+                            var(Y_L_graph-graph$y)),
+                    row.names = c("isoExp","alpha1","GL"))
+print(result)
+
+
 fig <- plot_obs(graph,Y_L_graph, y_loc = Y_loc) + scale_colour_gradientn(colours = heat.colors(10))
 print(fig)
