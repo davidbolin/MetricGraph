353f8b402fcb35cf56b03d003d766174baf19047
davidbolin <davidbolin@gmail.com>
2023-02-22 01:07:48 +0300

    update merge_line example with mesh building example

--- examples/merge_lines.R
+++ examples/merge_lines.R
@@ -1,14 +1,7 @@
 library(sp)
 library(MetricGraph)
 
-n <- 100
-line1 <- Line(rbind(c(0,0),c(1,0)))
-line2 <- Line(rbind(c(1,0),c(2,0)))
-lines = sp::SpatialLines(list(Lines(list(line1),ID="1"),
-                              Lines(list(line2),ID="2")))
-graph <- metric_graph$new(lines = lines,tolerance = list(vertex_vertex = 0, vertex_line = 0, line_line = 0),
-                          remove_deg2 = TRUE)
+graph <- metric_graph$new(tolerance = list(vertex_vertex = 1e-1, vertex_line = 1e-3, line_line = 1e-3),
+                          remove_deg2 = FALSE)
 graph$plot(degree = TRUE)
-
-plot(graph$lines)
-lines(graph$lines[1],col=2)
+graph$build_mesh(h = 0.01)
