026ae7477fb17375b467dfef65d5309570064863
davidbolin <davidbolin@gmail.com>
2023-03-02 11:00:54 +0300

    update example spacetime

--- examples/space_time.R
+++ examples/space_time.R
@@ -5,7 +5,7 @@ library(htmlwidgets)
 
 save.plot <- FALSE
 
-line1 <- Line(rbind(c(0,0),c(1,0)))
+line1 <- Line(rbind(c(1,0),c(0,0)))
 line2 <- Line(rbind(c(0,0),c(0,1)))
 line3 <- Line(rbind(c(0,0),c(0,-1)))
 line4 <- Line(rbind(c(0,1),c(1,0)))
@@ -20,7 +20,58 @@ h = 0.01
 graph$build_mesh(h = h)
 graph$compute_fem()
 
-kappa <- 10
+make.Q <- function(L0,L,dt,C, T) {
+  n <- dim(L)[1]
+  Lbar = C + dt*L
+  CC = kronecker(Diagonal(n=T), dt*C)
+  LL <- Matrix(0,nrow = T*n, ncol = T*n)
+  LL[1:n,1:n] <- L0
+  for(t in 1:(T-1)) {
+    LL[(t*n+1):((t+1)*n),(t*n+1):((t+1)*n)] <- Lbar
+    LL[(t*n+1):((t+1)*n),((t-1)*n+1):(t*n)] <- -C
+  }
+  return(list(L = LL, C = CC))
+}
+
+kappa <- 15
+rho <- 0
+sigma <- 75
+dt <- 0.2
+n <- dim(graph$mesh$C)[1]
+L <- graph$mesh$G + kappa^2*graph$mesh$C + rho*graph$mesh$B
+L0 <- graph$mesh$C
+T = 200
+Q <- make.Q(L0, L, dt, graph$mesh$C, T)
+
+k <- 50
+v1 <- rep(0,dim(Q$L)[1]); v1[k] <- 1
+v2 <- rep(0,dim(Q$L)[1]); v2[n+k] <- 1
+v3 <- rep(0,dim(Q$L)[1]); v3[floor(T/2)*n+k] <- 1
+v4 <- rep(0,dim(Q$L)[1]); v4[(T-1)*n+k] <- 1
+
+c1 <- sigma^2*solve(Q$L,Q$C%*%solve(t(Q$L),v1))[1:n]
+c2 <- sigma^2*solve(Q$L,Q$C%*%solve(t(Q$L),v2))[(n+1):(2*n)]
+c3 <- sigma^2*solve(Q$L,Q$C%*%solve(t(Q$L),v3))[(floor(T/2)*n+1):((floor(T/2)+1)*n)]
+c4 <- sigma^2*solve(Q$L,Q$C%*%solve(t(Q$L),v4))[((T-1)*n+1):(T*n)]
+
+L <- graph$mesh$G + kappa^2*graph$mesh$C + rho*graph$mesh$B
+
+v1 <- rep(0,dim(L)[1]); v1[k] <- 1
+ct <- sigma^2*solve(L,graph$mesh$C%*%solve(t(L),v1))/dt
+p <- graph$plot_function(as.vector(c1), plotly = TRUE, support_width = 0, line_color = "red")
+p <- graph$plot_function(as.vector(c2), plotly = TRUE, p = p, support_width = 0, line_color = "green")
+p <- graph$plot_function(as.vector(c3), plotly = TRUE, support_width = 0, line_color = "blue", line_width = 2)
+p <- graph$plot_function(as.vector(c4), plotly = TRUE, p = p,support_width = 0, line_color = "gray", line_width = 2)
+p <- graph$plot_function(as.vector(ct*max(c4)/max(ct)), plotly = TRUE, p = p, support_width = 1, line_color = "black")
+p
+cat(max(ct)/max(c4))
+
+
+
+
+
+###########
+kappa <- 20
 rho <- 1
 n <- dim(graph$mesh$C)[1]
 C <- graph$mesh$C
@@ -35,25 +86,33 @@ r <- solve(Q,t(A))
 vars <- diag(solve(Q))
 graph$plot_function(r,plotly = TRUE)
 
-kappa <- 1
-rho <- -500
-sigma <- 100
-dt <- 0.25*h^2
+kappa <- 0.01
+rho <- 0
+sigma <- 1000
+dt <- 0.005
+n <- dim(graph$mesh$C)[1]
 I <- Diagonal(n,1)
+C <- graph$mesh$C
+h <- rowSums(C)
+C <- Diagonal(h,n = n)
 L <- graph$mesh$G + kappa^2*C + rho*graph$mesh$B
 
+
 u0 <- rep(0,n)
-u0[1] <- 1
-T <- 10000
+u0[1] <- 0
+T <- 20
 U <- matrix(0,nrow=n,ncol = T)
 U[,1] <- u0
 
 for(i in 1:(T-1)){
-  U[,i+1] <- as.vector((I - dt*L)%*%U[,i] + dt*sigma*rnorm(n, sd = h))
+  noise <- dt*sigma*rnorm(n, sd = h)
+  U[,i+1] <- as.vector(solve((C + dt*L), C%*%U[,i] + noise))
 }
-fig <- graph$plot_movie(U[,seq(from=1,to=10000,by=1000)])
+fig <- graph$plot_movie(U)
+fig
 if(save.plot){
   htmlwidgets::saveWidget(fig, file = "spacetime.HTML", selfcontained = TRUE)
 }
+
 
 
