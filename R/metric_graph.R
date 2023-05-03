45bbaf416360fb0a671588445a22de21c033718c
vpnsctl <alexandre.impa@gmail.com>
2023-05-03 04:59:40 +0300

    Namespace + small bugfix

--- R/metric_graph.R
+++ R/metric_graph.R
@@ -696,7 +696,7 @@ metric_graph <-  R6::R6Class("metric_graph",
     index_order <- order(tmp_df$group, tmp_df$PtE1, tmp_df$PtE2)
     self$data <- lapply(self$data, function(dat){ dat[index_order]})
 
-    self$PtV <- self$PtV[index_order]
+    self$PtV <- self$PtV[index_order[1:length(self$PtV)]]
 
     private$temp_PtE <- NULL
 
