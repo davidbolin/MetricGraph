5e493e8e143f8e2e70f1a4140a075d6437957369
JonasWallin <jonas.wallin81@gmail.com>
2025-01-08 15:00:36 +0100

    test

--- R/psp_to_metricgraph.R
+++ R/psp_to_metricgraph.R
@@ -1,49 +1,58 @@
 
-
-
+#' Convert an `stlpp` object to a metric graph object
 #'
-#' function to export a stlpp object, from stlnpp, to metric graph object
+#' This function converts an `stlpp` object (from the `stlnpp` package) into a metric graph object.
 #'
-#'@export
-stlpp.to.graph <- function(stlpp.obj){
-  graph <- linnet.to.graph(stlpp.obj$domain)
-  data <- as.data.frame(stlpp.obj$data)
-  graph$add_observations(data = data.frame(y = rep(1, dim(data)[1]),
-                                           time = data[,3],
-                                           coord_x = data[,1],
-                                           coord_y = data[,2]),
-                         coord_x= "coord_x",
-                         coord_y = "coord_y",
-                          data_coords ="spatial")
-  return(graph)
+#' @param stlpp.obj An `stlpp` object to be converted.
+
+#' @return A metric graph object
+#' @export
+stlpp.to.graph <- function(stlpp.obj,...) {
+  return(linnet.to.graph(stlpp.obj$domain,...))
 }
 
-#' from
-#' function to export a linnet object, from spatstat package, to metric graph object
+#' Convert a `linnet` object to a metric graph object
 #'
-#'@export
-linnet.to.graph <- function(linnet.object){
+#' This function converts a `linnet` object (from the `spatstat` package) into a metric graph object.
+#'
+#' @param linnet.object A `linnet` object to be converted.
+#'
+#' @return A metric graph object with edges defined by the network.
+#' @export
+linnet.to.graph <- function(linnet.object) {
   n <- length(linnet.object$from)
   vertices <- as.data.frame(linnet.object$vertices)
   lines <- vector("list", n)
-  for(i in 1:n){
-    lines[[i]] <- rbind(vertices[linnet.object$from[i],1:2],
-                        vertices[linnet.object$to[i],1:2])
+
+  for (i in 1:n) {
+    lines[[i]] <- rbind(
+      vertices[linnet.object$from[i], 1:2],
+      vertices[linnet.object$to[i], 1:2]
+    )
   }
+
   return(metric_graph$new(edges = lines))
 }
 
+#' Convert a `psp` object to a metric graph object
 #'
-#' function to export a psp object, from spatstat package, to metric graph object
+#' This function converts a `psp` object (from the `spatstat` package) into a metric graph object.
 #'
-#'@export
-psp.to.graph <- function(psp.object){
-
-  n <- dim(psp.object$ends)[1]
+#' @param psp.object A `psp` object to be converted.
+#'
+#' @return A metric graph object with edges defined by the segments in the `psp` object.
+#' @export
+psp.to.graph <- function(psp.object) {
+  n <- nrow(psp.object$ends)
   lines <- vector("list", n)
-  for(i in 1:n){
-    lines[[i]] <- rbind(as.matrix(psp.object$ends[i,1:2]),as.matrix(psp.object$ends[i,3:4]))
+
+  for (i in 1:n) {
+    lines[[i]] <- rbind(
+      as.matrix(psp.object$ends[i, 1:2]),
+      as.matrix(psp.object$ends[i, 3:4])
+    )
   }
+
   graph <- metric_graph$new(edges = lines)
   return(graph)
 }
